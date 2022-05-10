a47ebb7d9e9c182c5408ca9d3bf3ff750ba58169
Mustafa <matterik971@gmail.com>
2022-05-10 16:25:30 +0300

    0.2.0

---
+++ sciter.d.ts
@@ -0,0 +1,417 @@
+//| Sciter.d.ts v0.2.0
+//| https://github.com/MustafaHi/sciter-declarations
+
+
+declare module "@sciter" {
+    export const VERSION: string;
+    export const REVISION: string;
+    export const QUICKJS_VERSION: string;
+//    export function import(path: string): object;
+    /**
+     * Load native Sciter extension
+     * @param name path to library without .dll/.dylib (relative to sciter.dll)
+     */
+    export function loadLibrary(name: string): any;
+    /** Passive json parser */
+    export function parseValue(val:string): any;
+    export function devicePixels(length: number | string)
+    /** Generate unique id */
+    export function uuid(): string;
+    export function encode(text: string, encoding ?: string): ArrayBuffer;
+    export function decode(bytes: ArrayBuffer, encoding ?: string): string;
+    export function compress(input: ArrayBuffer, method?: "gz" | "gzip" | "lzf"): ArrayBuffer;
+    export function decompress(input: ArrayBuffer, method?: "gz" | "gzip" | "lzf"): ArrayBuffer;
+    export function toBase64(input:ArrayBuffer): string;
+    export function fromBase64(input:string): ArrayBuffer;
+    export function md5(input:ArrayBuffer): string;
+    export function crc32(input:ArrayBuffer): number;
+}
+
+declare module "@env" {
+    export const OS: string;
+    export const PLATFORM: string;
+    export const DEVICE: "desktop" | "mobile";
+    export function language(): string;
+    export function country(): string;
+    export function userName(): string;
+    export function machineName(): string;
+    export function domainName(): string;
+    /**
+     * Launch file/URL with default system application
+     */
+    export function launch(path:string): void;
+    export function home(relpath ?: string): string;
+    export function homeURL(relpath ?: string): string;
+    /**
+     * Return path of default system folder ie. documents|downloads
+     * @param name of default system folder
+     * @param relpath relative path to name
+     */
+    export function path(name: keyof typeof systemPath, relpath ?: string): string;
+    export function pathURL(name: string): string;
+    /**
+     * Execute comma seperated arguments
+     */
+    export function exec(...args: string[]): void;
+    
+}
+
+declare enum systemPath { "home", "root", "desktop", "applications", "downloads", "documents", "music", "videos", "pictures" }
+
+
+declare module "@sys" {
+    declare interface spawnOptions {stdout?: string, stdin?: string, stderr?: string}
+    export function spawn(args: array<string>, options?: spawnOptions ): Process;
+    export function hrtime();
+    export function gettimeofday();
+    export function uname();
+    export function isatty();
+    export function environ();
+    export function getenv();
+    export function setenv();
+    export function unsetenv();
+    export function cwd(): string;
+    export function homedir(): string;
+    export function tmpdir();
+    export function exepath();
+    export function random();
+
+    namespace fs {
+        /**
+         * Watch directory for changes
+         * @param path 
+         * @param cb callback function
+         */
+        function watch(path:string, cb: (path:string, events: 0x01 | 0x02) => WatchFS);
+        /**
+         * Open file instance
+         * @param path 
+         * @param flags method to open the file with read/write
+         * @param mode file content encoding
+         */
+        function open(path:string, flags: keyof typeof OpenFlagOptions, mode ?: number): Promise<File>;
+        /**
+         * Synchronously open file instance
+         * @param path 
+         * @param flags method to open the file with read/write
+         * @param mode file content encoding
+         */
+        function $open(path:string, flags: keyof typeof OpenFlagOptions, mode ?: number): File;
+        function stat(path:string): Promise<StatStruct>;
+        function $stat(path:string): StatStruct;
+        function lstat(): Promise<StatStruct>;
+        function $lstat(): StatStruct;
+        /** Remove file */
+        function unlink(path:string): Promise;
+        function rename(oldpath:string, newpath: string) : Promise;
+        function mkdtemp(template:string) : Promise<string>;
+        function mkstemp(template:string) : Promise<string>;
+        function rmdir(path:string) : Promise;
+        function $rmdir(path:string);
+        function mkdir(path:string, mode ?: 0o777): Promise;
+        function $mkdir(path:string, mode ?: 0o777);
+        function copyfile(): Promise;
+        function readdir(path: string): Promise<FileList>;
+        function $readdir(path: string): FileList;
+        /** Read file content, check `$readfile` for sync method */
+        function readfile(path: string): Promise<ArrayBuffer>;
+        /** Synchronously read file content */
+        function $readfile(path: string): ArrayBuffer;
+        
+    }
+    
+    interface Dir {
+        close();
+        path: string;
+        next();
+        [async iterator];
+    }
+
+    declare interface File {
+        read (length?:number, fileposition ?: number): Promise<Uint8Array>;
+        $read(length?:number, fileposition ?: number): Uint8Array;
+        write (data:string|string[]|ArrayBuffer, filePosition ?: number) : Promise<number>;
+        $write(data:string|string[]|ArrayBuffer, filePosition ?: number) : number;
+        close (): Promise<undefined>;
+        $close(): undefined;
+        fileno(): number;
+        stat(): Promise<Object>;
+        path: string;
+    }
+
+    declare interface WatchFS {
+        readonly path: string;
+        close(): void;
+    }
+
+    declare interface StatStruct {
+        isFile ?: boolean;
+        isDirectory ?: boolean;
+        isSymbolicLink ?: boolean;
+        st_dev: number;
+        st_ino: number;
+        st_mode: number;
+        st_nlink: number;
+        st_uid: number;
+        st_gid: number;
+        st_rdev: number;
+        st_size: number;
+        st_blksize: number;
+        st_blocks: number;
+        st_atime: number;
+        st_mtime: number;
+        st_ctime: number;
+        st_birthtime: number;
+    }
+}
+
+declare enum OpenFlagOptions { 'a', 'ax', 'a+', 'ax+', 'as', 'as+', 'r', 'r+', 'rs+', 'w', 'wx', 'w+', 'wx+' }
+
+declare interface Process {
+    kill();
+    wait(): Promise<ProcessStats>;
+    pid: number;
+    stdin: Pipe;
+    stdout: Pipe;
+    stderr: Pipe;
+}
+
+declare interface ProcessStats {
+    exit_status: number;
+    term_signal: number;
+}
+
+declare interface Socket {
+    close();
+    read();
+    write();
+    fileno();
+}
+declare interface Pipe extends Socket {
+    listen();
+    accept();
+    getsockname();
+    getpeername();
+    connect();
+    bind();
+}
+
+declare interface TTY extends Socket {
+    setMode();
+    getWinSize();
+}
+
+declare interface UDPSocket extends Socket {
+    close();
+    recv();
+    send();
+    getsockname();
+    getpeername();
+    connect();
+    bind();
+}
+
+declare interface TCPSocket {
+    shutdown();
+    fileno();
+    listen();
+    accept();
+    getsockname();
+    getpeername();
+    connect();
+    bind();
+}
+
+
+/**
+ * Format arguments using [C-style printf conventions](https://en.cppreference.com/w/cpp/io/c/fprintf).  
+ * Sciter specific:  
+    `%v` - print as JSON.stringify(arg);  
+    `%V` - print as JSON.stringify(arg, null, " ");
+ */
+declare function printf(...args: string[]): string;
+
+/**
+ * Format arguments using [C-style scanf conventions](https://en.cppreference.com/w/c/io/fscanf).  
+ */
+declare function scanf(...args: string[]): array<string | number>;
+
+/** Number of physical screen pixels in logical CSS px (dip) */
+declare var devicePixelRatio: float;
+
+interface Element {
+    /** querySelector */
+    $(query: string): Element;
+    /** querySelectorAll */
+    $$(query: string): array<Element>;
+
+    /* NATIVE */
+
+    /** Get element matching the css selector */
+    querySelector(query: string): Element;
+    /** Get array of elements matching the css selector */
+    querySelectorAll(query: string): array<Element>;
+    firstElementChild: Element;
+    lastElementChild: Element;
+    nextElementSibling: Element;
+    previousElementSibling: Element;
+    childElementCount: number;
+    children: array<Element>;
+    childElement(index: number): Element;
+
+    appendChild(node: Node);
+    removeChild(node: Node);
+    insertBefore(node: Node, refNode: Node);
+    insertAfter(node: Node, refNode: Node);
+    replaceChild(newNode: Node, refNode: Node);
+    insertAdjacentHTML(where: number, html: string);
+    swapWith(element: Element);
+
+
+    readonly clientLeft: number;
+    readonly clientTop: number;
+    readonly clientWidth: number;
+    readonly clientHeight: number;
+    readonly scrollLeft: number;
+    readonly scrollRight: number;
+    readonly scrollWidth: number;
+    readonly scrollHeight: number;
+    
+    style: Style;
+    state: State;
+
+    disabled: boolean;
+    readonly: boolean;
+    checked: boolean;
+    src: string;
+
+    classList: {
+        add(name: string[], value: string|number): void;
+        remove(name: string[]): void;
+        toggle(name: string, state?: boolean): boolean;
+        contains(name: string): boolean;
+        length: number;
+        readonly entries(): array<string>;
+    }
+
+    id: string;
+    name: string;
+    tagName: string;
+    tag: string;
+    className: string;
+    elementIndex: number;
+    innerHTML: string;
+    outerHTML: string;
+    innerText: string;
+    value: string|number|boolean|undefined;
+
+}
+
+
+interface Document extends Element {
+    /** Load image from `url` and bind it to variable */
+    bindImage(url: string, img ?: Graphics.Image): Graphics.Image;
+
+    /** return path relative to document path */
+    url(relpath ?: string): string;
+
+
+    /* NATIVE */
+
+    body: Element;
+    head: Element;
+    /** Root(html) element */
+    documentElement: Element;
+    /** document loading state - `complete` | `interactive` */
+    readyState: string;
+    createElement(tag: string): Element;
+    createTextNode(): Node;
+    createComment(): Comment;
+    createDocumentFragment();
+    createNodeIterator(root: string, whatToShow?: string, filter?: string): NodeIterator;
+}
+
+declare var Document: {
+    new(): Document;
+};
+
+declare var document: Document;
+
+interface Window {
+    // new(param: object<windowParam>);
+    //
+    new(): Window;
+    state: number|windowState;
+
+    /** Window has input focus. */
+    readonly isActive: boolean;
+
+    /** Monitor index where the current window is on */
+    readonly screen: number;
+    /** Number of monitors in the system */
+    readonly screens: number;
+
+    /** current graphics backend used: `direct2d`, `Skia/OpenGL`, etc. */
+    readonly graphicBackend: string;
+    /** blur-behind effect  
+     * one of `none` `auto` `dark` `ultra-dark` `light` `ultra-light` */
+    blurBehind: string;
+
+    /** Minimal size of resizable window `[width, height]` */
+    minSize: array;
+    /** Maximum size of resizable window `[width, height]` */
+    maxSize: array;
+
+    frameType: frameType;
+}
+
+declare var Window: {
+    this: Window;
+    new(param?: windowParam): Window;
+}
+
+declare var window: Window;
+
+interface windowParam {
+    type?: number|windowType;
+    /** When owner closed or minimized this window will be closed/minimized too. */
+    parent?: Window;
+    /** window caption (or title) */
+    caption?: string;
+    x?: number;
+    y?: number;
+    width?: number;
+    height?: number;
+    /** x,y,w,h are coordinates of desired window client box on the screen; */
+    client?: boolean;
+    /** [1 to 9] alignment of the window on monitor,
+     * if [-1 to -9] and parent is provided then it aligns the window against parent window.
+     * (`1` bottom left corner, `2` bottom middle, `3` bottom right corner,
+     * `4` middle left, `5` center, `6` middle right, `7` top left corner,
+     * `8` top middle, `9` top right corner) */
+    alignment?: number;
+    /** index of monitor to spawn on. */
+    screen?: number;
+    state?: number|windowState;
+    /** window html source file */
+    url?: string;
+    /** extra parameters to pass to the new window. */
+    parameter?: array|string|object;
+}
+
+enum windowType {
+    POPUP_WINDOW  = 0,
+    TOOL_WINDOW   = 1,
+    CHILD_WINDOW  = 2,
+    FRAME_WINDOW  = 3,
+    DIALOG_WINDOW = 4,
+}
+enum windowState {
+    WINDOW_SHOWN     = 0,
+    WINDOW_MINIMIZED = 1,
+    WINDOW_MAXIMIZED = 2,
+    WINDOW_HIDDEN    = 3,
+    WINDOW_FULL_SCREEN = 4,
+}
+
+enum frameType { "standard", "solid", "solid-with-shadow", "extended", "transparent" }
